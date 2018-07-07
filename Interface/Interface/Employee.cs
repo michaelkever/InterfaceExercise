7cd3f86094227b50a7854eb46ba5a523004c0b2e
Michael Kever <michaelkever@gmail.com>
2018-07-06 19:08:04 -0500

    Interface exercise for the tech academy
    exercise on page 76

---
+++ Interface/Interface/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Employee : Person, IQuittable
+
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: [{FirstName} {LastName}]");
+        }
+
+        void IQuittable.Quit()
+        {
+            throw new NotImplementedException();
+        }
+        public string IQuittable();
+        {
+            Console.ReadLine(IQuittable);
+        }
+    }
+
+}
